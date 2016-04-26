a76172ca3ba7d72c4e8c7f1b22bbb7047473eae4
Mark Havekes <mark.havekes@gmail.com>
2016-04-26 22:46:53 +0200

    add basic layout with SplitPane

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage window) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/layout/MainView.fxml"));
+        window.setTitle("WordFeud");
+        window.setScene(new Scene(root));
+        window.show();
+
+        //set the minimal window size to the preferred calculated size
+        window.setMinWidth(window.getWidth());
+        window.setMinHeight(window.getHeight());
+    }
+
+    public static void main(String[] args) { launch(args); }
+}
