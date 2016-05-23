2650d7b40e23406b94a291d09b5a8c180fc909eb
Mark Havekes <mark.havekes@gmail.com>
2016-05-23 15:58:32 +0200

    got rid of code duplication

--- src/views/CompetitionInfoView.java
+++ src/views/CompetitionInfoView.java
@@ -1,15 +1,13 @@
 package views;
 
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.chart.XYChart;
 import models.Competition;
 
 public class CompetitionInfoView extends View {
 
-    private int totalGames;
     @FXML private Label competitionName;
     @FXML private Label competitionInfo;
     @FXML private BarChart<String,Integer> gameChart;
@@ -25,47 +23,30 @@ public class CompetitionInfoView extends View {
         });
     }
 
-    public void setInfo(Competition competition){
+    private void setInfo(Competition competition){
         competitionName.setText(competition.getName());
         competitionInfo.setText("Eigenaar: " + competition.getOwner().getName());
-        totalGames = competition.getAmountOfGames();
-        int totalPlayers = competition.getAmountOfUsers();
-        int averageScoreInComp = competition.getCompetitionScoreAvgerage();
-        joinButton.setVisible(true);
 
-        //game chart
-        gameChart.getData().clear();
-        gameChart.setBarGap(10);
-        gameChart.setVerticalGridLinesVisible(false);
-        gameChart.setAnimated(false);
-        XYChart.Series<String,Integer> gameSeries = new XYChart.Series<>();
-        gameSeries.getData().add(new XYChart.Data<>("Totaal spellen", totalGames));
-        gameChart.getData().add(gameSeries);
-
-        //player chart
-        playerChart.getData().clear();
-        playerChart.setBarGap(10);
-        playerChart.setVerticalGridLinesVisible(false);
-        playerChart.setAnimated(false);
-        XYChart.Series<String,Integer> playerSeries = new XYChart.Series<>();
-        playerSeries.getData().add(new XYChart.Data<>("Totaal spelers", totalPlayers));
-        playerChart.getData().add(playerSeries);
-
-        //score chart
-        scoreChart.getData().clear();
-        scoreChart.setBarGap(10);
-        scoreChart.setVerticalGridLinesVisible(false);
-        scoreChart.setAnimated(false);
-        XYChart.Series<String,Integer> scoreSeries = new XYChart.Series<>();
-        scoreSeries.getData().add(new XYChart.Data<>("Gemiddelde score", averageScoreInComp));
-        scoreChart.getData().add(scoreSeries);
+        prepareChart(gameChart, "Totaal spellen", competition.getAmountOfGames());
+        prepareChart(playerChart, "Totaal spelers", competition.getAmountOfUsers());
+        prepareChart(scoreChart, "Gemiddelde score", competition.getCompetitionScoreAvgerage());
 
         if (competitionController.isUserInCompetition(session.getCurrentUser(), competition)) {
             joinButton.setVisible(false);
         }
     }
 
-    @FXML public void joinCompetition(){
+    private void prepareChart(BarChart<String, Integer> chart, String label, int value) {
+        chart.setVerticalGridLinesVisible(false);
+        chart.setAnimated(false);
+        XYChart.Series<String,Integer> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(label , value));
+        chart.getData().clear();
+        chart.getData().add(series);
+    }
+
+    @FXML
+    public void joinCompetition(){
         if (!competitionController.isUserInCompetition(session.getCurrentUser(), competitionController.getSelectedCompetition())) {
             if (competitionController.addUserInCompetition(session.getCurrentUser(), competitionController.getSelectedCompetition())){
                 refresh();
