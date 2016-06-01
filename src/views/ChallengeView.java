0bf6c8312309aad8afa3f209864bb03df27fdc21
Dave <pjhkaste@avans.nl>
2016-06-01 15:08:42 +0200

    added stuff

--- src/views/ChallengeView.java
+++ src/views/ChallengeView.java
@@ -19,16 +19,35 @@ public class ChallengeView extends View {
         User receiver = userController.getSelectedUser();
 
         if (!languageBox.getSelectionModel().isEmpty()) {
-            if (!gameController.challenge(languageBox.getValue(), requester, receiver, comp)) {
-                challenge.setTextFill(Color.RED);
-                challenge.setText("Fout met uitnodigen");
-            } else{
+            int feedback = gameController.challenge(languageBox.getValue(), requester, receiver, comp);
+            setFeedback(feedback);
+        } else {
+            this.setFeedback(4);
+        }
+    }
+
+    private void setFeedback(int feedback) {
+        switch (feedback) {
+            case 0:
                 challenge.setTextFill(Color.GREEN);
                 challenge.setText("Succesvol uitgedaagd");
-            }
-        } else {
-            challenge.setTextFill(Color.RED);
-            challenge.setText("Er is geen taal aangewezen");
+                break;
+            case 1:
+                challenge.setTextFill(Color.RED);
+                challenge.setText("U kunt uzelf niet uitnodigen");
+                break;
+            case 2:
+                challenge.setTextFill(Color.RED);
+                challenge.setText("U speelt al een game met deze persoon");
+                break;
+            case 3:
+                challenge.setTextFill(Color.RED);
+                challenge.setText("U zit niet in dezelfde competitie");
+                break;
+            case 4:
+                challenge.setTextFill(Color.RED);
+                challenge.setText("Er is geen taal aangewezen");
+                break;
         }
     }
 
