713c333a1dfc5294f3e71e3499badaf0db66927c
Jesse <jessewijnakker@gmail.com>
2016-06-02 12:37:03 +0200

    Centered button and added some style to button and window

--- src/views/SwapTileView.java
+++ src/views/SwapTileView.java
@@ -3,13 +3,19 @@ package views;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import main.Main;
 import models.Tile;
 import resources.ResourceFactory;
 import views.components.FieldTileNode;
@@ -18,6 +24,7 @@ public class SwapTileView extends View {
     ResourceFactory resourceFactory;
     FieldTileNode field;
     ObservableList<FieldTileNode> returnvalue;
+    MainView mainView;
     @Override
     public void refresh() {
         // TODO Auto-generated method stub
@@ -35,15 +42,20 @@ public class SwapTileView extends View {
        
     }
     
-    public SwapTileView(ResourceFactory resourceFactory){
+    public SwapTileView(MainView mainView, ResourceFactory resourceFactory){
         this.resourceFactory = resourceFactory;
+        this.mainView = mainView;
        
     }
     
     public ObservableList<FieldTileNode> swapTiles(ObservableList<Tile> currentRack){
         Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
+        window.setMaxHeight(200);
+        window.setMaxWidth(640);
         window.initStyle(StageStyle.UNDECORATED);
+        window.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) window.close();
+        });
         
         TilePane tilePane = new TilePane();
         tilePane.setOnDragDetected(event -> event.consume());
@@ -51,7 +63,7 @@ public class SwapTileView extends View {
         tilePane.setHgap(10);
         tilePane.setVgap(10);
         tilePane.setPrefColumns(7);
-        tilePane.setStyle("-fx-background-color: gray");
+        tilePane.setStyle("-fx-background-color: gray;");
         ObservableList<Tile> tiles = currentRack;
         ObservableList<FieldTileNode> selectedTiles = FXCollections.observableArrayList();
         for(Tile tile: currentRack){
@@ -68,31 +80,34 @@ public class SwapTileView extends View {
                 }
             });
         }
-      
+             
         Button swap  = new Button("Ruilen");
-        Button close = new Button("Sluiten");
-        
-        tilePane.getChildren().addAll(close, swap);
-        
-        close.setOnAction(event -> window.close());
+        swap.setStyle("-fx-background-color: white; -fx-font-size: 18px;");
         swap.setOnAction(event -> { 
-            
             returnvalue = selectedTiles;
             window.close();
         });
         
-       Scene scene = new Scene(tilePane);
+        HBox hBox = new HBox();
+        hBox.setStyle("-fx-background-color: gray;");
+        hBox.getChildren().add(swap);
+        hBox.setAlignment(Pos.BOTTOM_CENTER);
+       
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(tilePane, hBox);
+        vBox.setStyle("-fx-background-color: gray; -fx-border-color: white; -fx-border-width: 4px;");
+        vBox.setPadding(new Insets(0,0, 5, 0));
+        
+       Scene scene = new Scene(vBox);
        window.setScene(scene);
+       window.setX(mainView.gameBoardView.getScene().getWindow().getX() + mainView.gameBoardView.getScene().widthProperty().intValue() /2 - window.getMaxWidth()/2);
+       window.setY(mainView.gameBoardView.getScene().getWindow().getY() + mainView.gameBoardView.getScene().heightProperty().intValue() /2 - window.getMaxHeight()/2);
        window.showAndWait(); 
        
        if(returnvalue == null){
            return null;
        }
        return returnvalue;
-       
-       
     }
-   
-
 }
 
