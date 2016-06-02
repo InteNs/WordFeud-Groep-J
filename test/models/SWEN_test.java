9f7e88f72ae4fad1ebf64fa0d6e78249daf037f7
jeroenLu <jeroenluers@gmail.com>
2016-06-02 19:51:35 +0200

    test 1-3 refactored

--- test/models/SWEN_test.java
+++ test/models/SWEN_test.java
@@ -8,6 +8,9 @@ import enumerations.BoardType;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 public class SWEN_test {
 
 
@@ -32,18 +35,35 @@ public class SWEN_test {
 
     // Manier van Jeroen
     @Test
-    public void fieldsChanged(){
+    public void testPath1_noTilesAdded(){
+        turnbuilder.verifyAndCalculate();
+        assertTrue("no tiles are added", turnbuilder.getTilesChangedThisTurn().size() == 0);
+        assertTrue("score is 0", turnbuilder.getScore() == 0);
+    }
 
-        turnbuilder.getGameBoard()[7][7].setTile(t('A'));
-        turnbuilder.getGameBoard()[7][8].setTile(t('A'));
-        turnbuilder.getGameBoard()[7][9].setTile(t('P'));
+    @Test
+    public void testPath2_tilesNotPlacedOverStartTile(){
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[9][9],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[9][10],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[9][11],t('P'));
+
+        assertTrue("tiles are added", turnbuilder.getTilesChangedThisTurn().size() > 0);
+        assertNull("turn is not valid", turnbuilder.verifyCurrentTurn());
+        assertTrue("score is 0", turnbuilder.getScore() == 0);
 
         printGameBoard();
+    }
 
+    @Test
+    public void testPath3_wordNotInOneLine(){
+        // A valid word is not on 1 axis
         turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[8][8],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[8][9],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[9][10],t('P'));
 
-        //prints debug info(score + wordsFound
-        turnbuilder.verifyAndCalculate();
+        assertTrue("tiles are added", turnbuilder.getTilesChangedThisTurn().size() > 0);
+        assertNull("turn is not valid", turnbuilder.verifyCurrentTurn());
+        assertTrue("score is 0", turnbuilder.getScore() == 0);
 
         printGameBoard();
     }
