e55fa69a4db2b647482c3168245e57b5751b5d06
FreshFromHolland <christophferran@hotmail.com>
2016-06-03 14:42:23 +0200

    tests added

--- test/models/SWEN_test.java
+++ test/models/SWEN_test.java
@@ -29,7 +29,12 @@ public class SWEN_test {
     private Tile t(char character) {
         switch (character) {
             case 'A': return new Tile('A', 1);
+            case 'C': return new Tile('C', 5);
+            case 'J': return new Tile('J', 4);
+            case 'I': return new Tile('I', 2);
             case 'P': return new Tile('P', 4);
+            case 'U': return new Tile('U', 2);
+            case 'Z': return new Tile('Z', 5);
         }
         return null;
     }
@@ -109,6 +114,45 @@ public class SWEN_test {
         printGameBoard();
     }
 
+    @Test
+    public void testPath7_isWordConneted(){
+        // A valid word is not on 1 axis
+        turnbuilder.getGameBoard()[7][7].setTile(t('A'));
+        turnbuilder.getGameBoard()[7][8].setTile(t('A'));
+        turnbuilder.getGameBoard()[7][9].setTile(t('P'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[8][9],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[9][9],t('P'));
+
+        assertTrue("tiles are added", turnbuilder.getTilesChangedThisTurn().size() > 0);
+        assertTrue("turn is not valid", turnbuilder.verifyCurrentTurn() != null );
+        assertTrue("score is 0", turnbuilder.getScore() == 0);
+
+        printGameBoard();
+    }
+
+    @Test
+    public void testPath8_extraScoreApplied(){
+        // A valid word is not on 1 axis
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][2],t('J'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][3],t('A'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][4],t('C'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][5],t('U'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][6],t('Z'));
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][7],t('Z'));
+
+        assertTrue("turn is not valid", turnbuilder.verifyCurrentTurn() != null );
+        turnbuilder.verifyAndCalculate();
+        System.out.println(turnbuilder.getScore());
+
+        turnbuilder.addPlacedTile(turnbuilder.getGameBoard()[7][8],t('I'));
+
+        assertTrue("turn is not valid", turnbuilder.verifyCurrentTurn() != null );
+        turnbuilder.verifyAndCalculate();
+        System.out.println(turnbuilder.getScore());
+
+        assertTrue("tiles are added", turnbuilder.getScore() == 2*(4+1+5+2+5+5+2)+40);
+        printGameBoard();
+    }
 
     private void printGameBoard() {
         for(int i = 0; i < turnbuilder.getGameBoard().length; i ++){
@@ -116,7 +160,7 @@ public class SWEN_test {
                 if(x == (turnbuilder.getGameBoard().length -1)){
                     System.out.println(turnbuilder.getGameBoard()[ i ][ x ].toString());
                 } else {
-                    System.out.print(turnbuilder.getGameBoard()[ i ][ x ].toString());
+                    System.out.print(turnbuilder.getGameBoard()[ i ][ x ].toString() + " ");
                 }
 
             }
