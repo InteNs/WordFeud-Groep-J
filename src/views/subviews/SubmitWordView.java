e9187be3446a0f14aca2984f052c956a8a9978a0
Cedric Remond <cedricremond@live.nl>
2016-06-03 17:38:01 +0200

    styling window

--- src/views/subviews/SubmitWordView.java
+++ src/views/subviews/SubmitWordView.java
@@ -4,20 +4,21 @@ import enumerations.Language;
 import enumerations.WordStatus;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.*;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import models.User;
 import models.Word;
+import views.MainView;
 import views.View;
 
 import java.util.ArrayList;
@@ -29,57 +30,76 @@ public class SubmitWordView extends View {
     private Stage window = new Stage();
     private ArrayList<Word> wordList = new ArrayList<>();
 
-    public SubmitWordView(ArrayList<String> words, Language gameLanguage, User user) {
+    public SubmitWordView(ArrayList<String> words, Language gameLanguage, User user, MainView mainView) {
         this.gameLanguage = gameLanguage;
         owner = user;
         cbList = new ArrayList<>();
+
         window.initModality(Modality.WINDOW_MODAL);
         window.initStyle(StageStyle.UNDECORATED);
-        TilePane tilePane = new TilePane();
-        tilePane.setMaxHeight(400);
-        tilePane.setHgap(10);
-        tilePane.setVgap(10);
-        tilePane.setStyle("-fx-background-color: white");
-        tilePane.setOnMouseClicked(event -> window.close());
 
+        TitledPane titledPane = new TitledPane();
+        titledPane.setCollapsible(false);
+        titledPane.setText("Woorden indienen");
+        titledPane.setTextAlignment(TextAlignment.CENTER);
 
+        VBox rootVbox = new VBox();
+        rootVbox.setPadding(new Insets(25, 25, 25, 25));
 
-        VBox vbox = new VBox();
-        vbox.setAlignment(Pos.TOP_CENTER);
-        Label label1 = new Label("Selecteer de woorden die je wil indienen.");
-        vbox.getChildren().add(label1);
+        window.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) window.close();
+        });
+
+        Label label1 = new Label();
+        if(words.size() > 1){
+            label1.setText("Deze woorden zijn niet geldig! \n" +
+                    "Selecteer de woorden die je wil indienen.");
+        } else {
+            label1.setText(("Dit word niet geldig! \n" +
+                    "Selecteer het als je het wil indienen."));
+        }
+        label1.setTextAlignment(TextAlignment.CENTER);
+
+        rootVbox.getChildren().add(label1);
+        rootVbox.setAlignment(Pos.CENTER);
         for (String w : words) {
             Label label = new Label(w.toLowerCase());
+            label.setPadding(new Insets(10, 10, 10, 10));
             CheckBox checkBox = new CheckBox();
             checkBox.setUserData(w);
-            HBox hbox = new HBox();
-            hbox.setAlignment(Pos.CENTER);
             checkBox.setPadding(new Insets(10, 0, 0, 10));
             label.setPadding(new Insets(10, 0, 0, 10));
-            hbox.getChildren().addAll(label, checkBox);
-            vbox.getChildren().add(hbox);
+            HBox wordBox = new HBox(label, checkBox);
+            wordBox.setAlignment(Pos.CENTER);
+            rootVbox.getChildren().addAll(wordBox);
             cbList.add(checkBox);
         }
+
         Button bConfirm = new Button("Bevestig");
-        vbox.getChildren().add(bConfirm);
-        Scene scene = new Scene(vbox);
+        HBox buttonBox = new HBox(bConfirm);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setPadding(new Insets(20, 0, 10, 0));
+
+        rootVbox.getChildren().add(buttonBox);
+        titledPane.setContent(rootVbox);
+        Scene scene = new Scene(titledPane);
         bConfirm.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 submitWords();
             }
         });
+
         window.setScene(scene);
+        window.setX(mainView.gameBoardView.getScene().getWindow().getX() + mainView.gameBoardView.getScene().getWidth() / 2 + 85);
+        window.setY(mainView.gameBoardView.getScene().getWindow().getY() + mainView.gameBoardView.getHeight() / 2);
         window.showAndWait();
-        window.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) window.close();
-        });
     }
 
     private void submitWords() {
         for (CheckBox cb : cbList) {
             if (cb.isSelected()) {
-                Word word = new Word(cb.getUserData().toString().toLowerCase(), owner.getName(), gameLanguage.toString(), WordStatus.PENDING);
+                Word word = new Word(cb.getUserData().toString(), owner.getName(), gameLanguage.toString(), WordStatus.PENDING);
                 wordList.add(word);
             }
             window.close();
